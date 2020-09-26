4eab1935a6968d7119b9425b9e3150e62f7aa268
sarahabimay <sarahabimay@gmail.com>
2020-09-26 10:21:13 +0100

    Move config files to directories

--- mysymlinks.sh
+++ mysymlinks.sh
@@ -4,6 +4,19 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
+########## Function to create symlink
+create_link_if_missing() {
+  local source=$1
+  local target=$2
+  local target_file=$(basename $target)
+
+  if [ ! -f $target ]; then
+    echo "Linking $target_file"
+    ln -s $source $target
+  else
+    echo "Skipping $target_file"
+  fi
+}
 ########## Variables
 
 dir=~/dotfiles                        # dotfiles directory
@@ -22,11 +35,22 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
   echo "Moving any existing dotfiles from ~ to $olddir"
   mv ~/.$file ~/dotfiles_old/
-  echo "Creating symlink to $file in home directory."
-  ln -s $dir/$file ~/.$file
+  # echo "Creating symlink to $file in home directory."
+  # ln -s $dir/$file ~/.$file
 done
 
+echo "Creating symlink to $file in home directory."
+create_link_if_missing $dir/zsh/zshrc ~/.zshrc
+create_link_if_missing $dir/bash/bashrc ~/.bashrc
+create_link_if_missing $dir/bash/bash_profile ~/.bash_profile
+create_link_if_missing $dir/bash/bash_aliases ~/.bash_aliases
+create_link_if_missing $dir/git/gitconfig ~/.gitconfig
+create_link_if_missing $dir/git/gitignore_global ~/.gitignore_global
+create_link_if_missing $dir/git/gitconfig ~/.gitconfig
+create_link_if_missing $dir/vim/vimrc ~/.vimrc
+create_link_if_missing $dir/vim/ ~/.vim
+create_link_if_missing $dir/tmux/tmux.conf ~/.tmux.conf
